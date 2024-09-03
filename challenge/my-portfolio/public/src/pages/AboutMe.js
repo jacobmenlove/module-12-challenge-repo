6a865e1352b64946e0f7a8cdb6c42f3447a702ad
Jacob Menlove <jacobmenlove1@gmail.com>
2024-09-03 17:04:08 -0400

    5th push. about me element

---
+++ challenge/my-portfolio/public/src/pages/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+function AboutMe() {
+    return (
+        <section id="about-me">
+        <img src="./challenge/Assets/IMG_4158.jpg" alt="Your Avatar" />
+        <p>I am a web developer. I began my web development journey in June 2024.</p>
+      </section>
+    );
+  }
+  
+  export default AboutMe;
